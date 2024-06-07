6fada4007567e42fc54b3f4b32951755fb79153d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-08 00:59:29 +0900

    feat: polararea 그래프 수정 컴포넌트화 완료

--- components/chart/polararea-chart.js
+++ components/chart/polararea-chart.js
@@ -1,7 +1,11 @@
 import { useRef, useEffect } from "react";
 import { Chart } from "chart.js/auto";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-export default function PolarAreaChart() {
+Chart.register(ChartDataLabels);
+
+// PolarArea 그래프
+export default function PolarAreaChart({ labels, label, data }) {
   const chartRef = useRef(null);
 
   useEffect(() => {
@@ -15,28 +19,28 @@ export default function PolarAreaChart() {
       const newChart = new Chart(context, {
         type: "polarArea",
         data: {
-          labels: ["John", "Jane", "Doe"],
+          labels: labels,
           datasets: [
             {
-              label: "Info",
-              data: [34, 64, 23],
+              label: label,
+              data: data,
               backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-                "rgba(255, 205, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(201, 203, 207, 0.2)",
+                "rgba(255, 99, 132, 0.8)",
+                "rgba(255, 159, 64, 0.8)",
+                "rgba(255, 205, 86, 0.8)",
+                "rgba(75, 192, 192, 0.8)",
+                "rgba(54, 162, 235, 0.8)",
+                "rgba(153, 102, 255, 0.8)",
+                "rgba(201, 203, 207, 0.8)",
               ],
               borderColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-                "rgba(255, 205, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(201, 203, 207, 0.2)",
+                "rgba(255, 99, 132, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(255, 205, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(201, 203, 207, 1)",
               ],
               borderWidth: 1,
             },
@@ -44,12 +48,29 @@ export default function PolarAreaChart() {
         },
         options: {
           responsive: true,
+          plugins: {
+            datalabels: {
+              color: "#fff",
+              anchor: "center",
+              align: "end",
+              offset: 5,
+              borderRadius: 8,
+              backgroundColor: function (context) {
+                return context.dataset.backgroundColor;
+              },
+              formatter: (value, context) => {
+                return `${
+                  context.chart.data.labels[context.dataIndex]
+                }: ${value}`;
+              },
+            },
+          },
         },
       });
 
       chartRef.current.chart = newChart;
     }
-  }, []);
+  }, [labels, label, data]);
 
   return (
     <div style={{ position: "relative", width: "90vw", height: "80vh" }}>

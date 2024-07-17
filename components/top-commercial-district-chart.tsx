33ad99de2da80fb245b98423ed418084a83338ae
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-17 23:24:33 +0900

    refactor: 인기상권차트 컴포넌트 memo 적용 변경

--- components/top-commercial-district-chart.tsx
+++ components/top-commercial-district-chart.tsx
@@ -1,27 +1,26 @@
 "use client";
 
 import DoughnutChart from "@/components/chart/doughnut-chart";
-import { hangjeongDongState } from "@/recoil/atoms";
-import { useEffect, useState } from "react";
+import { useEffect, useState, memo } from "react";
 import { useRecoilValue } from "recoil";
 import { getNearbyTopComCategories } from "@/_actions/getNearbyTopComCategories";
-import { sliceHangjeongDongCode } from "@/utils/slice-hangjeong-dong-code";
 import { concatHangjeongDongName } from "@/utils/concat-hangjeong-dong-name";
 import { ITopCommercialDistrict } from "@/types/commercial_district";
+import { hangjeongDongCodeSelector } from "@/recoil/selector";
 
 // 인기 업종 차트 섹션
-export default function TopCommercialDistrictChart() {
-  const hangjeongDong = useRecoilValue(hangjeongDongState);
+export default memo(function TopCommercialDistrictChart() {
+  const hangjeongDongCode = useRecoilValue(hangjeongDongCodeSelector);
   const [topComDistrictCategories, setTopComDistrictCategories] = useState<
     ITopCommercialDistrict[]
   >([]);
 
-  // 중심 위치가 변경될 때마다 상위 상권 분류 정보 받아옴
+  // 행정동 코드가 변경될 때마다 상위 상권 분류 정보 받아옴
   useEffect(() => {
-    if (hangjeongDong) {
+    if (hangjeongDongCode) {
       (async () => {
         const { data, error } = await getNearbyTopComCategories(
-          sliceHangjeongDongCode(hangjeongDong?.code)
+          hangjeongDongCode
         );
         if (data) {
           setTopComDistrictCategories(data);
@@ -30,23 +29,27 @@ export default function TopCommercialDistrictChart() {
         }
       })();
     }
-  }, [hangjeongDong]);
+  }, [hangjeongDongCode]);
 
   return (
     <section className={"PopulationChartWrapper w-full overflow-auto"}>
-      <label className={"my-4 font-semibold text-[1.2rem]"}>
-        {concatHangjeongDongName(hangjeongDong)}의 인기 업종
-      </label>
-      <div className="w-full flex flex-col items-center gap-1">
-        <DoughnutChart
-          label={"상가 갯수"}
-          labels={topComDistrictCategories.map(
-            (category) => category.standardCategory
-          )}
-          data={topComDistrictCategories.map((category) => category.count)}
-          unit={"개"}
-        />
-      </div>
+      {hangjeongDongCode && topComDistrictCategories && (
+        <>
+          <label className={"my-4 font-semibold text-[1.2rem]"}>
+            {/* {concatHangjeongDongName(hangjeongDong)}의 인기 업종 */}
+          </label>
+          <div className="w-full flex flex-col items-center gap-1">
+            <DoughnutChart
+              label={"상가 갯수"}
+              labels={topComDistrictCategories.map(
+                (category) => category.standardCategory
+              )}
+              data={topComDistrictCategories.map((category) => category.count)}
+              unit={"개"}
+            />
+          </div>
+        </>
+      )}
     </section>
   );
-}
+});

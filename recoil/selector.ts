6999ef2a6c84cbd94b32b4d71877441b8ce37b8b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-17 23:00:42 +0900

    feat: 행정동코드 추출하는 recoil selector 추가

--- recoil/selector.ts
+++ recoil/selector.ts
@@ -1,13 +1,24 @@
 import { selector } from "recoil";
-import { nearbyComDistrictState } from "./atoms";
+import { nearbyComDistrictState, hangjeongDongState } from "./atoms";
 
 // 근처 상권 가게 정보
 export const nearbyStoresSelector = selector({
   key: "nearbyStoresSelector",
   get: ({ get }) => {
     const nearbyComDistrict = get(nearbyComDistrictState);
+
     return nearbyComDistrict.flatMap((category) =>
       category.smallCategories.flatMap((smallCategory) => smallCategory.stores)
     );
+  },
+});
+
+// 행정동 코드 추출
+export const hangjeongDongCodeSelector = selector({
+  key: "hangjeongdongSelector",
+  get: ({ get }) => {
+    const hangjeongDong = get(hangjeongDongState);
+
+    return Number(hangjeongDong.code?.slice(0, -2));
   },
 });

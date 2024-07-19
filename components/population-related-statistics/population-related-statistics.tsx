771b554d2f0dbef89f0e137b22ea38b17f8ecb54
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-20 00:17:33 +0900

    feat: 렌더링 예외 조건 추가

--- components/population-related-statistics/population-related-statistics.tsx
+++ components/population-related-statistics/population-related-statistics.tsx
@@ -1,16 +1,23 @@
 import { useRecoilValue } from "recoil";
-import PopulationChart from "./population-by-hour-chart";
 import SalesByDemographicsChart from "../sales-by-demographics-chart";
-import { hangjeongDongState } from "@/recoil/atoms";
+import {
+  hangjeongDongState,
+  selectedAgeIdxState,
+  selectedGenderIdxState,
+  selectedHourIdxState,
+} from "@/recoil/atoms";
 import PopulationByHourChart from "./population-by-hour-chart";
 import PopulationInnerHour from "./population-inner-hour";
 
 export default function PopulationRelatedStatistics() {
   const hangjeongDong = useRecoilValue(hangjeongDongState);
 
+  if (!hangjeongDong) {
+    return <h1>선택된 행정동이 없습니다. 지도 위에 마커를 표시해주세요.</h1>;
+  }
   return (
     <>
-      {hangjeongDong && <PopulationByHourChart />}
+      <PopulationByHourChart />
       <PopulationInnerHour />
       <SalesByDemographicsChart />
     </>

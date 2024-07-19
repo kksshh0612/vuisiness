6b49cdb8880b7ad4a3636f80be39d0a9c42f4dab
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-20 00:01:06 +0900

    refactor: 행정동 이름 결합, 시군구 이름 추출 함수 recoil selector로 변경

--- recoil/selector.ts
+++ recoil/selector.ts
@@ -1,5 +1,6 @@
 import { selector } from "recoil";
 import { nearbyComDistrictState, hangjeongDongState } from "./atoms";
+import { removeSpaces } from "@/utils/remove-spaces";
 
 // 근처 상권 가게 정보
 export const nearbyStoresSelector = selector({
@@ -15,10 +16,28 @@ export const nearbyStoresSelector = selector({
 
 // 행정동 코드 추출
 export const hangjeongDongCodeSelector = selector({
-  key: "hangjeongdongSelector",
+  key: "hangjeongDongCodeSelector",
   get: ({ get }) => {
     const hangjeongDong = get(hangjeongDongState);
 
     return Number(hangjeongDong.code?.slice(0, -2));
+  },
+});
+
+// 행정동 이름 결합
+export const hangjeongDongNameSelector = selector({
+  key: "hangjeongDongNameSelector",
+  get: ({ get }) => {
+    const hangjeongDong = get(hangjeongDongState);
+    return `${hangjeongDong?.region_2depth_name} ${hangjeongDong?.region_3depth_name} ${hangjeongDong?.region_4depth_name}`;
+  },
+});
+
+// 시군구 이름 추출
+export const SiGunGuNameSelector = selector({
+  key: "SiGunGuNameSelector",
+  get: ({ get }) => {
+    const hangjeongDong = get(hangjeongDongState);
+    return removeSpaces(`${hangjeongDong?.region_2depth_name}`);
   },
 });
--- utils/concat-hangjeong-dong-name.ts
+++
@@ -1,9 +0,0 @@
-import { removeSpaces } from "./remove-spaces";
-
-export const concatHangjeongDongName = (hangjeongDong) => {
-  return `${hangjeongDong?.region_2depth_name} ${hangjeongDong?.region_3depth_name} ${hangjeongDong?.region_4depth_name}`;
-};
-
-export const extractSiGunGuName = (hangjeongDong) => {
-  return removeSpaces(`${hangjeongDong?.region_2depth_name}`);
-};

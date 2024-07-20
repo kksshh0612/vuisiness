aef7aa5c5a407fd74c4ab2a474076e46d3dd53b8
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-20 15:58:26 +0900

    feat: 인기상권 차트 로딩상태 추가

--- components/top-commercial-district-chart.tsx
+++ components/top-commercial-district-chart.tsx
@@ -4,26 +4,33 @@ import DoughnutChart from "@/components/chart/doughnut-chart";
 import { useEffect, useState, memo } from "react";
 import { useRecoilValue } from "recoil";
 import { getNearbyTopComCategories } from "@/_actions/getNearbyTopComCategories";
-import { concatHangjeongDongName } from "@/utils/concat-hangjeong-dong-name";
 import { ITopCommercialDistrict } from "@/types/commercial_district";
-import { hangjeongDongCodeSelector } from "@/recoil/selector";
+import {
+  hangjeongDongCodeSelector,
+  hangjeongDongNameSelector,
+} from "@/recoil/selector";
 
 // 인기 업종 차트 섹션
 export default memo(function TopCommercialDistrictChart() {
   const hangjeongDongCode = useRecoilValue(hangjeongDongCodeSelector);
+  const hangjeongDongName = useRecoilValue(hangjeongDongNameSelector);
   const [topComDistrictCategories, setTopComDistrictCategories] = useState<
     ITopCommercialDistrict[]
   >([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  console.log("dd");
   // 행정동 코드가 변경될 때마다 상위 상권 분류 정보 받아옴
   useEffect(() => {
     if (hangjeongDongCode) {
       (async () => {
+        setIsLoading(true);
         const { data, error } = await getNearbyTopComCategories(
           hangjeongDongCode
         );
         if (data) {
           setTopComDistrictCategories(data);
+          setIsLoading(false);
         } else {
           console.error(error);
         }
@@ -31,12 +38,16 @@ export default memo(function TopCommercialDistrictChart() {
     }
   }, [hangjeongDongCode]);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <section className={"PopulationChartWrapper w-full overflow-auto"}>
       {hangjeongDongCode && topComDistrictCategories && (
         <>
           <label className={"my-4 font-semibold text-[1.2rem]"}>
-            {/* {concatHangjeongDongName(hangjeongDong)}의 인기 업종 */}
+            {hangjeongDongName}의 인기 업종
           </label>
           <div className="w-full flex flex-col items-center gap-1">
             <DoughnutChart

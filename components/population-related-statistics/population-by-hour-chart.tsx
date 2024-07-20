64b67317de0526339e075116ea83a79800e671c2
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-20 15:57:00 +0900

    feat: 시간별 생활인구 차트 요일선택란 추가

--- components/population-related-statistics/population-by-hour-chart.tsx
+++ components/population-related-statistics/population-by-hour-chart.tsx
@@ -15,8 +15,8 @@ export default function PopulationByHourChart() {
   const hangjeongDongName = useRecoilValue(hangjeongDongNameSelector);
   const [activePopulation, setActivePopulation] = useState();
   const [isLoading, setIsLoading] = useState(false);
-
-  const [day, setDay] = useState("금");
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+  const [selectedDay, setSelectedDay] = useState(days[new Date().getDay()]);
 
   // 중심 위치가 변경될 때, 생활 인구 정보 받아옴
   useEffect(() => {
@@ -26,7 +26,7 @@ export default function PopulationByHourChart() {
         const { data: activePopulationData, error } =
           await getActivePopulations({
             hangjeongDongCode,
-            day,
+            day: selectedDay,
           });
         if (activePopulationData) {
           setActivePopulation(activePopulationData);
@@ -36,7 +36,7 @@ export default function PopulationByHourChart() {
         }
       })();
     }
-  }, [hangjeongDongCode, day]);
+  }, [hangjeongDongCode, selectedDay]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -48,8 +48,34 @@ export default function PopulationByHourChart() {
         {activePopulation?.length !== 0 ? (
           <>
             <label className={"font-semibold mt-[2rem] text-[1.2rem]"}>
-              {hangjeongDongName}의 {day}요일 생활 인구
+              {hangjeongDongName}의 {selectedDay}요일 생활 인구
             </label>
+            <div
+              className={
+                "w-full h-[5rem] flex flex-row justify-between items-center"
+              }
+            >
+              {days.map((day, idx) => {
+                let selected = selectedDay === day;
+                return (
+                  <span
+                    key={idx}
+                    className={`font-semibold w-[2.5rem] h-[2.5rem] rounded-full border-2 border-primary 
+                      hover:text-white hover:bg-primary hover:cursor-pointer text-primary 
+                      flex justify-center items-center
+                      ${
+                        selected
+                          ? "bg-primary text-white"
+                          : "bg-white text-primary"
+                      }
+                      `}
+                    onClick={() => setSelectedDay(day)}
+                  >
+                    {day}
+                  </span>
+                );
+              })}
+            </div>
             <div className={"w-full overflow-auto"}>
               <div className={"flex flex-row gap-2"}>
                 {activePopulation?.map((activePopulationItem, index) => {
